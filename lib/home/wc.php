1ded3442bdedbcd554b1b1672aa29371ab28ddf4
avijit sarkar <avisofficeone@gmail.com>
2018-03-15 03:16:22 +0530

    lib included to the repo who don't like to see it's working

---
+++ lib/home/wc.php
@@ -0,0 +1,24 @@
+<div class="ins-wrapper">
+    <section>
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <div class="jumbotron jumbotron-fluid">
+                        <div class="container">
+                                <h1 class="display-3"> wp-seed </h1>
+                                <p class="lead">wellcome , see what we got </p>
+                                <hr class="my-2">
+                                <p> <em>feel free to customise <em> </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+<section>
+
+    <h1> other Content </h1>
+
+</section>
+</div>
