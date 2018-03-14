40fb60ed9765e1516422a1d655ced1efe71f1eed
avijit sarkar <avisofficeone@gmail.com>
2018-03-15 01:57:30 +0530

    added admin menu entry and icon

---
+++ func/add_wc_page.php
@@ -0,0 +1,30 @@
+<?php 
+
+
+add_action( 'admin_menu' , 'ins_add_menu_homePage' );
+
+function ins_add_menu_homePage(){
+    $incPath = explode( "\\", plugin_dir_path( __FILE__ ) );
+    // $incPath = plugin_dir_path( __FILE__ );
+    print_r( $incPath );
+    add_menu_page(
+        // $page_title:string, 
+        __( 'wt-seed wellcome page', 'inset'),
+        // $menu_title:string, 
+        __( 'wt-seed', 'inset'),
+        // $capability:string, 
+        'manage_options',
+        // $menu_slug:string,
+        'ins_wt-seed',
+        // $function:callable, 
+        'ins_wellcome_page_setup',
+        // $icon_url:string,
+        plugins_url( $incPath [count( $incPath ) -2 ] )."/inc/img/ins_.png"
+        // $position:integer|null
+    );
+}
+
+function ins_wellcome_page_setup(){
+    echo 'hi';
+}
+
