6e6886fe2879e50e8afb4ae0087f71ff3d5e6750
avijit sarkar <avisofficeone@gmail.com>
2018-03-15 01:11:54 +0530

    no need for functions.php

--- functions.php
+++
@@ -1,8 +0,0 @@
-<?php 
-/*|###########################################################\
-| | this will be a index file of all our functions          | |
-| | create-> funcitons on ./func/ then require_once ( here )| |
-| | thanks : avijit sakrar                                  | |
-\ |#########################################################|*/
-
-require_once (get_template_directory() . "/func/enqueue.php");
